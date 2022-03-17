23392103cf638489c4159cdbbea57bb8865a997f
Thiago Neves <thiago.neves@kroonar.com>
2022-03-17 17:30:43 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { listen } = require('express/lib/application');
+
+const app = express();
+const portNumber = 3000;
+
+app.listen(portNumber, () => console.log('🔥 Server started at http://localhost:' + portNumber));
