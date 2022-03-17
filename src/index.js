e23558b8ac5b4157b540c811267e74752dea69eb
Thiago Neves <thiago.neves@kroonar.com>
2022-03-17 17:39:36 -0300

    using nodemon to hotreload serevr

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const { listen } = require('express/lib/application');
 const app = express();
 const portNumber = 3000;
 
-app.listen(portNumber, () => console.log('🔥 Server started at http://localhost:' + portNumber));
+app.listen(portNumber, () => console.log('🔥 Server started at http://localhost:' + portNumber + ' with a hot🥵 reload'));
