8c4562e13fd3f9eb3208ae776442c5f7c33eb3fb
Thiago Neves <thiago.neves@kroonar.com>
2022-03-17 22:49:33 -0300

    eslint + editor.config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { listen } = require('express/lib/application');
 
 const app = express();
 const portNumber = 3000;
 
-app.listen(portNumber, () => console.log('🔥 Server started at http://localhost:' + portNumber + ' with a hot🥵 reload'));
+app.listen(portNumber, () => console.log(
+  `🔥 Server started at http://localhost:${portNumber} with a hot🥵 reload`,
+));
